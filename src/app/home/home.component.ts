756047a05e6b10cec914ec54a101ec00eadc56a4
Ajeeb <ajeebajb@gmail.com>
2020-02-20 12:12:10 +0530

    add state dropdown and underscore.js

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { HomeService } from './home.service';
 import { ConfirmModalService } from '../core/confirm-modal/confirm-modal.service';
 import { MapsAPILoader, MouseEvent } from '@agm/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import * as _ from 'underscore';
 // import {} from '@types/googlemaps';
 declare const google: any;
 
@@ -16,30 +17,15 @@ declare const google: any;
 })
 export class HomeComponent implements OnInit, AfterViewInit {
 
-  filteredData: any;
-  filterPipeInstance = new FilterPipe();
-  listFilter: any;
   cityList: any = [];
-
-  @ViewChild('search', { static: false }) public searchElementRef: ElementRef;
   latitude: number;
   longitude: number;
   zoom = 15;
-
-  lat = 51.673858;
-  lng = 7.815982;
-  zm = 15;
-  geoCoder: any;
-  address: string;
+  states: any;
+  filteredCities: any;
 
   constructor(
-    private renderer: Renderer2,
-    private gd: GlobalDataService,
-    private router: Router,
     private hs: HomeService,
-    private confirmDialogService: ConfirmModalService,
-    private mapsAPILoader: MapsAPILoader,
-    private ngZone: NgZone,
     private snackBar: MatSnackBar
   ) {
   }
@@ -55,12 +41,14 @@ export class HomeComponent implements OnInit, AfterViewInit {
   getInit() {
     this.hs.getCities().subscribe(res => {
       this.cityList = res;
+      this.states = _.uniq(this.cityList, 'State');
+      this.states = _.sortBy(this.states, 'State');
     });
   }
 
   getLatLong(data) {
     this.hs.getLatLong(data).subscribe(res => {
-      console.log(res);
+      // console.log(res);
       if (res.results.length) {
         this.latitude = res.results[0].geometry.location.lat;
         this.longitude = res.results[0].geometry.location.lng;
@@ -70,6 +58,10 @@ export class HomeComponent implements OnInit, AfterViewInit {
         });
       }
     });
+  }
+
+  loadCities(state) {
+    this.filteredCities = _.filter(this.cityList, (item) => item.State === state.State);
   }
 
   setCurrentLocation() {
